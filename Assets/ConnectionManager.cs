8341d068f4996f7f595814e65fdc14c0e06ab022
devlanger <langeerr7@gmail.com>
2022-10-30 21:15:47 +0100

    Added Game Client connection with SignalR on unity side

---
+++ Assets/ConnectionManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UI;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+public class ConnectionManager : MonoBehaviour
+{
+    HubConnection connection;
+    [SerializeField] private string ip = "https://localhost:7006/gameHub";
+
+    public void Start()
+    {
+        connection = new HubConnectionBuilder()
+            .WithUrl(ip)
+            .Build();
+
+        Connect();
+
+        connection.Closed += async (error) =>
+        {
+            Debug.Log("Disconnected");
+            //await connection.StartAsync();
+        };
+
+        connection.On<string>("ReceiveGame", (gameIp) =>
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                Debug.Log($"Received game: {gameIp}");
+            });
+        });
+    }
+
+    private async void Connect()
+    {
+        await connection.StartAsync();
+    }
+}
